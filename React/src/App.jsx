cefa09d22a9fd7c131bf7fded8b3fdf36c034239
stevesdiary <stephenoyeyemi@gmail.com>
2024-11-18 00:08:14 +0100

    Making a card component

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,16 +1,11 @@
-import Header from "./header";
-import Footer from "./Footer";
-import Food from "./Food";
-
+import Card from "./Card";
 function App() {
   return (
     <>
-      <Header/>
-      <Food/>
-      <Footer/>
+      <Card/>
+      <Card/>
     </>
-    
-  );
+  )
 }
 
 export default App
